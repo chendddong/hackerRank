bf53d68afa427c487c37d55f47674b85b3419669
Peter <dchen@albany.edu>
2017-05-26 15:11:00 -0400

    Add Solutions 17 - 18

---
+++ 30 Days of Coding/Day 16 - Exceptions - String to Integer/Exceptions - String to Integer.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        scan.close();
+
+        try {
+            int num = Integer.parseInt(str);
+            System.out.println(num);
+        } catch (NumberFormatException e) {
+            System.out.println("Bad String");
+        }
+    }
+}
+
+/*
+    Use try catch when something could be happening
+ */
